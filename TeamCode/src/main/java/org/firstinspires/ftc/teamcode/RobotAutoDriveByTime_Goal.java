865c84aa4b5ec0ef02e11e3a25691b7d645abd9b
jimbecile <142739158+jimbecile@users.noreply.github.com>
2026-02-01 09:08:52 -0800

    Changed blue heading to accomodate error, changed names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByTime_Goal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByTime_Goal.java
@@ -67,7 +67,7 @@ import java.util.concurrent.TimeUnit;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Goal Shooting", group="Robot")
+@Autonomous(name="Goal Shooting Short", group="Robot")
 public class RobotAutoDriveByTime_Goal extends LinearOpMode {
 
     /* Declare OpMode members. */
